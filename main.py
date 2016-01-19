bffa95eddafd5cbedea35517d8770d3a620586d2
xunxingyuan <xunxingyuan@gmail.com>
2016-01-19 15:41:37 +0800

    finish module

--- main.py
+++ main.py
@@ -13,13 +13,19 @@ class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
             (r"/", MainHandler),
+            (r"/recommended/",RecommendedHandler)
         ]
         settings = dict(
             template_path=os.path.join(os.path.dirname(__file__), "templates"),
             static_path=os.path.join(os.path.dirname(__file__), "static"),
+            ui_modules={'book': BookModule},
             debug=True,
         )
         tornado.web.Application.__init__(self, handlers, **settings)
+
+class BookModule(tornado.web.UIModule):
+    def render(self, book):
+        return self.render_string('modules/book.html', book=book)
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
@@ -27,6 +33,45 @@ class MainHandler(tornado.web.RequestHandler):
             "index.html",
             page_title = "Burt's Books | Home",
             header_text = "Welcome to Burt's Books!",
+        )
+class RecommendedHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render(
+            "recommended.html",
+            page_title="Burt's Books | Recommended Reading",
+            header_text="Recommended Reading",
+            books=[
+                {
+                    "title":"Programming Collective Intelligence",
+                    "subtitle": "Building Smart Web 2.0 Applications",
+                    "image":"/static/images/collective_intelligence.gif",
+                    "author": "Toby Segaran",
+                    "date_added":1310248056,
+                    "date_released": "August 2007",
+                    "isbn":"978-0-596-52932-1",
+                    "description":"<p>This fascinating book demonstrates how you "
+                        "can build web applications to mine the enormous amount of data created by people "
+                        "on the Internet. With the sophisticated algorithms in this book, you can write "
+                        "smart programs to access interesting datasets from other web sites, collect data "
+                        "from users of your own applications, and analyze and understand the data once "
+                        "you've found it.</p>"
+                },
+                {
+                    "title":"Programming Collective Intelligence",
+                    "subtitle": "Building Smart Web 2.0 Applications",
+                    "image":"/static/images/collective_intelligence.gif",
+                    "author": "Toby Segaran",
+                    "date_added":1310248056,
+                    "date_released": "August 2007",
+                    "isbn":"978-0-596-52932-1",
+                    "description":"<p>This fascinating book demonstrates how you "
+                        "can build web applications to mine the enormous amount of data created by people "
+                        "on the Internet. With the sophisticated algorithms in this book, you can write "
+                        "smart programs to access interesting datasets from other web sites, collect data "
+                        "from users of your own applications, and analyze and understand the data once "
+                        "you've found it.</p>"
+                }
+            ]
         )
 
 if __name__ == "__main__":
--- string_service.py
+++ string_service.py
@@ -21,24 +21,13 @@ class WrapHandler(tornado.web.RequestHandler):
          width = self.get_argument('width', 40)
          self.write(textwrap.fill(text, int(width)))
 
-class FrobHandler(tornado.web.RequestHandler):
-    def head(self, frob_id):
-        frob = retrieve_from_db(frob_id)
-        if frob is not None:
-            self.set_status(200)
-        else:
-            self.set_status(404)
-    def get(self, frob_id):
-        frob = retrieve_from_db(frob_id)
-        self.write(frob.serialize())
 
 if __name__ == "__main__":
     tornado.options.parse_command_line()
     app = tornado.web.Application(
         handlers=[
             (r"/reverse/(\w+)",ReverseHandler),
-            (r"/wrap",WrapHandler),
-            (r"/frob/(\d+)", FrobHandler)
+            (r"/wrap",WrapHandler)
         ]
     )
     http_server = tornado.httpserver.HTTPServer(app)
